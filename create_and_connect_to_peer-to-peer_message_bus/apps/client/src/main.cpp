0bd1826abd4d66c3b3071a811f486c43df5729ad
black-desk <me@black-desk.cn>
2024-08-28 19:42:48 +0800

    Update create_and_connect_to_peer-to-peer_message_bus

--- create_and_connect_to_peer-to-peer_message_bus/apps/client/src/main.cpp
+++ create_and_connect_to_peer-to-peer_message_bus/apps/client/src/main.cpp
@@ -11,50 +11,54 @@ auto main(int argc, char **argv) -> int
         QCoreApplication app(argc, argv);
         initQCoreApplication();
 
-        auto ret = QMetaObject::invokeMethod(QCoreApplication::instance(), []() {
-                auto conn = QDBusConnection::connectToPeer(
-                        "unix:path=" + getPeerToPeerSocketAddress(),
-                        "p2p DBus connection to server");
-                if (!conn.isConnected()) {
-                        qCritical() << conn.name()
-                                    << "is not connected:" << conn.lastError();
-                        QCoreApplication::exit(-1);
-                        return;
-                }
+        auto ret = QMetaObject::invokeMethod(
+                QCoreApplication::instance(),
+                []() {
+                        auto conn = QDBusConnection::connectToPeer(
+                                "unix:path=" + getPeerToPeerSocketAddress(),
+                                "p2p DBus connection to server");
+                        if (!conn.isConnected()) {
+                                qCritical()
+                                        << conn.name() << "is not connected:"
+                                        << conn.lastError();
+                                QCoreApplication::exit(-1);
+                                return;
+                        }
 
-                auto server =
-                        new io::github::blackdesk::QDBusP2PExample::Server(
-                                "",
-                                "/io/github/blackdesk/QDBusP2PExample/Server",
-                                conn, QCoreApplication::instance());
+                        auto server = new io::github::blackdesk::QDBusP2PExample::
+                                Server("",
+                                       "/io/github/blackdesk/QDBusP2PExample/Server",
+                                       conn, QCoreApplication::instance());
 
-                if (!server->isValid()) {
-                        qCritical() << server->lastError();
-                        QCoreApplication::exit(-1);
-                        return;
-                }
+                        if (!server->isValid()) {
+                                qCritical() << server->lastError();
+                                QCoreApplication::exit(-1);
+                                return;
+                        }
 
-                QObject::connect(server,
-                                 &io::github::blackdesk::QDBusP2PExample::
-                                         Server::TestSignal,
-                                 []() {
-                                         qInfo() << "TestSignal arrived";
-                                         QCoreApplication::exit();
-                                 });
+                        QObject::connect(
+                                server,
+                                &io::github::blackdesk::QDBusP2PExample::
+                                        Server::TestSignal,
+                                []() {
+                                        qInfo() << "TestSignal arrived";
+                                        QCoreApplication::exit();
+                                });
 
-                QJsonObject object;
-                object.insert("a", "b");
-                QJsonObject object1;
-                object1.insert("b", object);
-                auto reply = server->TestMethod(object1.toVariantMap());
-                reply.waitForFinished();
-                if (reply.isError()) {
-                        qCritical() << "TestMethod error.";
-                        QCoreApplication::exit(-1);
-                        return;
-                }
-                qInfo() << "reply:" << reply.value();
-        });
+                        QJsonObject object;
+                        object.insert("a", "b");
+                        QJsonObject object1;
+                        object1.insert("b", object);
+                        auto reply = server->TestMethod(object1.toVariantMap());
+                        reply.waitForFinished();
+                        if (reply.isError()) {
+                                qCritical() << "TestMethod error.";
+                                QCoreApplication::exit(-1);
+                                return;
+                        }
+                        qInfo() << "reply:" << reply.value();
+                },
+                Qt::QueuedConnection);
 
         Q_ASSERT(ret);
 
